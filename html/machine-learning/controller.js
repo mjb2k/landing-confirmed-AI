d865f2aca7d955002ee1975ed62b0634b7ac4914
BuildTools <unconfigured@null.spigotmc.org>
2022-06-09 14:36:10 -0400

    starting on the Deep Q-learning Algo, lot to learn about this here but should be interesting. This may mean we don't do a evolutionary method but instead deep learnign to train a single agent how to play the game. This might be better because it could be deployed to different levels

---
+++ html/machine-learning/controller.js
@@ -0,0 +1,79 @@
+/*
+This file will consist of the Deep-Q learning
+algorithm to control the player.
+
+Important stuff for the player to know:
+There are only three actions a player can do
+- leftThrust
+- rightThrust
+- fullThrust (both left and right)
+
+We want to reward the player based on two things:
+1. if it wins the game we reward
+2. the closer it gets to the objective
+3. the faster it gets to the objective
+
+The way Deep Q-Learning works is as follows:
+1. Game starts and the Q-value is randomly initialized
+2. agent collects current state (calculated based on our stuff)
+3. agent executes action based on said state
+4. After this, the AI is rewarded and updates its Q value based on
+the bellman equation
+5. repeat steps 3-4 until game over
+
+We want to minimize the lost
+
+https://towardsdatascience.com/how-to-teach-an-ai-to-play-games-deep-reinforcement-learning-28f9b920440a
+https://github.com/maurock/snake-ga/blob/master/DQN.py
+*/
+
+class Controller {
+    constructor(player) {
+        this.player = player;
+        this.state = [];
+        this.reward = 0;
+    }
+
+    /*
+        Returns the state of the player
+        Represented by X values:
+        1. Orientation (angle)
+        2. Velocity
+        3. Distance to objective
+        4. Danger close to objects above
+        5. Danger close to objects left
+        6. Danger close to objects below
+        7. Danger close to objects right
+        8. time
+    */
+    getState() {
+        this.state =
+                [
+                    this.player.mainBody.angle, 
+                    this.player.mainBody.velocity,
+                    this.player.distanceToObjective(),
+                    d.getTime() - startTime,
+                    DANGER, DANGER, DANGER, DANGER
+                ]
+    }
+
+
+    /*
+    The reward score will be based from 0-1
+    Rewards is as follows:
+    1. -X the distance the player is to the objective
+    2. -X the time the player took to reach the objective
+    3. +X if the player wins
+    4. -X if the player dies
+    */
+    setReward() {
+        if (this.player.dead) {
+            this.reward -= .1;
+        }
+        if (this.player.wintime > 1.5) {
+            this.reward += .25;
+        }
+        this.reward -= this.player.distanceToObjective()/10000;
+        this.reward -= (d.getTime() - startTime)/60;
+    }
+}
--- html/main.js
+++ html/main.js
@@ -4,11 +4,12 @@
 var engine;
 var render;
 var runner = Matter.Runner.create();
-var player;
+var players = []; // population
 var level;
 var objects = [];
 var objective; // a single body that is considered the objective
-var overlay
+const d = new Date();
+var starTime = d.getTime();
 
 // create the engine and render.
 function init() {
@@ -26,17 +27,11 @@ function init() {
     Matter.Runner.run(runner, engine);
     Matter.Render.run(render);
 
-        overlay = document.createElement('canvas'),
-        context = overlay.getContext('2d');
 
-        overlay.width = render.canvas.width;
-        overlay.height = render.canvas.height;
-
-        document.body.appendChild(overlay);
-
-
-    addPlayer();
     addLevel();
+    for (i =0; i < 100; i++)
+        players.push(addPlayer());
+    
 }
 
 function addPlayer() {
